913d6ad7a786499835b9f77a8380c3761d6d4151
Kevin Kane <kevinthomaskane@gmail.com>
2018-03-20 10:51:07 -0700

    changed port

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,29 @@
 
 
 var friendsList = require("../data/friends.js")
-console.log(friendsList)
-
 
 
 function apiRoutes(app){
     app.get("/api/friends", function(req, res) {
         return res.json(friends);
     });
+
+    app.get("/api", function(req, res){
+        return res.json(friendsList)
+    });
     
-    app.post("/api/newfriends", function(req, res) {
-        var newfriend = {
-           name: req.body.name,
-           photo: req.body.photo,
-           scores: req.body.scores
-        }
-        req.body;
+    app.post("/api/friends", function(req, res) {
+        console.log("this is the req.body: " + req.body)
+        // var newfriend = {
+        //    name: req.body.name,
+        //    photo: req.body.photo,
+        //    scores: req.body.scores
+        // }
+        var newFriend = req.body;
         // newfriend.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-        console.log(newfriend);
-        friends.push(newfriend);
-        res.json(newfriend);
+        console.log("this is the new friend object: " + newFriend);
+        friendsList.push(newFriend);
+        res.json(newFriend);
       });
 }
 
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var apiRoutes = require("./app/routing/apiRoutes.js")
 
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
